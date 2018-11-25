69f4b4b145e9187a77a40b8447f614ea675fe456
White Gryphon <whitegryphon@gmail.com>
2018-11-25 13:02:48 -0500

    Remove forgot password until email collected

--- public/assets/includes/showLogin.php
+++ public/assets/includes/showLogin.php
@@ -37,7 +37,7 @@
                     <label for="modal_login_remember" class="custom-control-label">Remember me</label>
                 </div>
                 <div class="text-center">
-                    <a class="btn btn-info" href="#forgot">Forgot your password?</a>
+                    <!-- <a class="btn btn-info" href="#forgot">Forgot your password?</a> -->
                     <a class="btn btn-dark " href="#register">Need to register?</a>
                 </div>
             </div>
